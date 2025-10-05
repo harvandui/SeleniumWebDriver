827c4080970db331f8c0f9e290a6a74e638a44e5
alexandergf <arbetle@hotmail.com>
2025-10-04 22:49:03 -0500

    Segundo cambio

--- AUTO/SeleniumWebDriver/src/test/java/pages/PaginaRegistro.java
+++ AUTO/SeleniumWebDriver/src/test/java/pages/PaginaRegistro.java
@@ -1,5 +1,12 @@
 package pages;
 
+import java.util.List;
+
 public class PaginaRegistro extends BasePage {
 
+    private String planDropdown = "//select[@id='cart_cart_item_attributes_plan_with_interval']";
+
+    public List<String> returnPlanDropdownValues() {
+        return getDropdownValues(planDropdown);
+    }
 }
